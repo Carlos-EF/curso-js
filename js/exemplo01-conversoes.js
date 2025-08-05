75b5b4515ed0279d6a58f415d01f533b62f97012
Carlos Eduardo Felix Junior <juhfelixx07@gmail.com>
2025-08-05 10:28:43 -0300

    Clonado pastas e arquivos da aula 2025-08-04.

--- js/exemplo01-conversoes.js
+++ js/exemplo01-conversoes.js
@@ -1,10 +1,11 @@
-function converterStringParaInt() {
+// string
+function converterStringParaInt(){
     const anoFabricacaoEntradaUsuario = "2024"; // string
     const anoFabricacao = parseInt(anoFabricacaoEntradaUsuario);
     alert("Ano Fabricação: " + anoFabricacao);
 }
 
-function exemploConverterStringParaFloat() {
+function exemploConverterStringParaFloat(){
     const precoDoCarro = "299800.10";
     const preco = parseFloat(precoDoCarro);
     alert("Preço: " + preco);
